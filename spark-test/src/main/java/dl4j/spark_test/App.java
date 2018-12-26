a058a23569a85beb66a7fb4288979d2a48f82fe5
pdl <pdl@192.168.101.31>
2018-12-26 10:36:51 +0100

    changed workload to pi estimation

--- spark-test/src/main/java/dl4j/spark_test/App.java
+++ spark-test/src/main/java/dl4j/spark_test/App.java
@@ -1,34 +1,45 @@
 package dl4j.spark_test;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
- 
+import org.apache.spark.sql.SparkSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
-* Hello world!
-*
-*/
+ * Hello world!
+ *
+ */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.setProperty("hadoop.home.dir", "C:\\Users\\pdl\\Downloads\\bin-master\\");
- 
-        SparkConf conf = new SparkConf().setAppName("firstSparkProject").setMaster("local");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        String path = "linecount.txt";
- 
-        System.out.println("Trying to open: " + path);
-        
-        Path p1 = Paths.get(".");
-        System.out.println(p1.toAbsolutePath());
-        
-        JavaRDD<String> lines = sc.textFile(path.toString());
-        System.out.println("Lines count: " + lines.count());
-        sc.stop();
-    }
-}
+
+	public static void main(String[] args) throws Exception {
+		
+		SparkSession spark = SparkSession
+				.builder()
+				.appName("JavaSparkPi")
+				.getOrCreate();
+
+		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+		int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
+		int n = 100000 * slices;
+		List<Integer> l = new ArrayList<>(n);
+		for (int i = 0; i < n; i++) {
+			l.add(i);
+		}
+
+		JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+		int count = dataSet.map(integer -> {
+			double x = Math.random() * 2 - 1;
+			double y = Math.random() * 2 - 1;
+			return (x * x + y * y <= 1) ? 1 : 0;
+		}).reduce((integer, integer2) -> integer + integer2);
+
+		System.out.println("Pi is roughly " + 4.0 * count / n);
+
+		spark.stop();
+	}
+}    
