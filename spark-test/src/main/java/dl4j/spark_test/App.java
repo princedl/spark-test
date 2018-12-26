321418e71d5fef8cfbd037b457459180b4ff65a3
pdl <pdl@192.168.101.31>
2018-12-26 12:29:35 +0100

    set slice to 100 numbers

--- spark-test/src/main/java/dl4j/spark_test/App.java
+++ spark-test/src/main/java/dl4j/spark_test/App.java
@@ -24,7 +24,7 @@ public class App
 		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
 		int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
-		int n = 100000 * slices;
+		int n = 100 * slices;
 		List<Integer> l = new ArrayList<>(n);
 		for (int i = 0; i < n; i++) {
 			l.add(i);
